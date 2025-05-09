b6d8e5f927051245b4fe985604fbea8c0460c443
Maram suliman <mms.bur@outlook.com>
2025-05-09 16:22:33 +0400

    Add files via upload

---
+++ emp/EmpClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2_maram
+{
+    public class EmpClass
+    {
+       
+        
+            public string FirstName;
+            public string LastName;
+            public string Email;
+            public double wage;
+            public double WorkingHours;
+            public const double Tax =0.03;
+
+        
+    }
+}
+
---
+++ emp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using project2_maram;
+
+class program
+{
+
+    static void Main(string[]args)
+
+    {
+
+        Console.WriteLine("employees informaion :");
+
+        EmpClass employee = new EmpClass();
+
+        employee.FirstName = " maram ";
+        employee.LastName = " suliman ";
+        employee.Email = " mms.bur@outlook.com ";
+        employee.wage = 2.5;
+        employee.WorkingHours = 2;
+
+        Console.WriteLine($" grwth salary is {employee.wage * employee.WorkingHours}");
+        Console.WriteLine($" net salay is {(employee.wage * employee.WorkingHours)- (employee.wage * employee.WorkingHours*EmpClass.Tax)}");
+
+    }
+}    
